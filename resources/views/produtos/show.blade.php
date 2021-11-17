a8efeade22de027800f5f1b898fc7ad0f5875ec7
eduardopireslucio1 <eduardopireslucioo@gmail.com>
2021-11-17 16:09:37 -0300

    view show produtos 1.0

---
+++ resources/views/produtos/show.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.admin')
+@section('title', $models_produtos->produtos)
+
+@section('content')
+
+<div class="col-md-10 offset-md-1">
+    <div class="row">
+        <div id="image-container" class="col-md-6">
+            <img src="/images/produtos/{{$models_produtos->imagem}}" class="img-fluid" alt="">
+        </div>
+        <div id="info-container" class="col-md-6">
+            <h1><strong>Nome do produto: <br></strong>{{$models_produtos->nome}}</h1>
+            <p class="produto-descricao"><br><strong>Descrição: </strong>{{$models_produtos->descricao}}</p>
+            <p class="produto-preco"><strong>Preço: </strong>{{$models_produtos->preco}}</p>
+            <p clas="produto-status"><strong>Status:  </strong>
+            
+            @if($models_produtos->status == 0)
+               Desativado
+            @else
+                    Ativo
+            
+            @endif
+            
+            
+            </p>
+            <p class="produto-material">
+            <strong>Materiais: </strong>
+            @foreach ($models_produtos->material as $material)
+                <li>{{$material}}</li>
+            @endforeach
+            </p>
+        </div>
+    </div>
+</div>
+
+
+@endsection
